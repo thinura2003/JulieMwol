c74cc326300626706c1c4b63b39867ac197daab5
King Dark Alpha <93303850+thinura2003@users.noreply.github.com>
2022-02-24 09:26:18 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -9,7 +9,7 @@ const Config = require('../config');
 const {MessageType} = require('@adiwajshing/baileys');
 
 const Language = require('../language');
-const Lang = Language.getString('_asena');
+const Lang = Language.getString('tgm');
 
 if (Config.WORKTYPE == 'private') {
 
