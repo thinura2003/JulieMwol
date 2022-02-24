4a1e285fed47ea27185a3aac137cec5d9630f292
King Dark Alpha <93303850+thinura2003@users.noreply.github.com>
2022-02-24 17:29:32 +0530

    Update bgm.js

--- plugins/bgm.js
+++ plugins/bgm.js
@@ -16,7 +16,7 @@ let baseURI = '/apps/' + config.HEROKU.APP_NAME;
     var BGM_on = ''
     var BGM_off = ''
     if (config.LANG == 'EN') {
-        l_dsc = 'turn on and turn off bgm. -bot owner command'
+        l_dsc = '*turn on and  turn off bgm. -bot owner command*'
         BGM_on = 'bgm option turned on!'
         BGM_off = 'bgm option turned off'
     }
